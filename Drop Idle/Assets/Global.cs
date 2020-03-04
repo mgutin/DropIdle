c231b8a73442e24c712f2a75ff16be6000976ccd
Michael Gutin <michael.gutin@gmail.com>
2020-03-04 16:13:07 -0700

    Everything
    All of it

---
+++ Drop Idle/Assets/Global.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Global : MonoBehaviour
+{
+    private static double money = 0;
+
+    private static GameObject protoBall = GameObject.Find("Ball");
+
+
+
+    public static List<Ball> balls = new List<Ball>();
+
+    public static void changeBallSizeProperty(float change)
+    {
+        
+        float newSize = protoBall.transform.localScale.x + change;
+        protoBall.transform.localScale = new Vector2(newSize, newSize);
+    }
+
+
+    public static void score(double amount)
+    {
+        money += amount;
+        UI.moneyText.GetComponent<Text>().text = "$" + money;
+    }
+
+    public void Update()
+    {
+        
+    }
+}
---
+++ Drop Idle/Assets/Spawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float cooldown = 0.1f;
+    public float curTime = 0;
+    public Vector3 spawnLocation;
+    public float chaosFactor = 10f;
+    public string ballName = "Ball";
+
+    private static int counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnLocation = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        curTime +=Time.deltaTime;
+        if(curTime > cooldown)
+        {
+            curTime -= cooldown;
+            spawn();
+        }
+    }
+
+    void spawn()
+    {
+        //Setup GameObject
+        GameObject newBallObject = GameObject.Instantiate(GameObject.Find(ballName), spawnLocation, Quaternion.identity);    
+        Transform sprite = newBallObject.transform.Find("Sprite");
+        Transform lighting = newBallObject.transform.Find("Lighting");
+        sprite.position = spawnLocation;
+        sprite.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-chaosFactor, chaosFactor), 0);
+        lighting.position = spawnLocation;
+
+
+        //Setup Ball class
+        Ball newBall = newBallObject.GetComponent<Ball>();
+        newBall.ballObject = newBallObject;
+        newBall.sprite = sprite;
+        newBall.lighting = lighting;
+        Global.balls.Add(newBall);
+    }
+}
